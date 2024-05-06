8df684ecdfd4e355da677521ebd1e786a2d356ce
RRTracer <rrtracer@gail.com>
2024-05-06 18:16:24 +0200

    ceci est une update

---
+++ git_save.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Ajouter tous les fichiers modifiés
+git add .
+
+# Effectuer un commit avec le message spécifié
+git commit -m "ceci est une update"
+
+# Pousser les modifications vers le dépôt distant
+git push
